7f2dd31996dc52fdc89130961aa71cf9b0ee3ff9
sarveshgupta1997 <sarveshgupta14@gmail.com>
2023-03-20 13:43:44 +0530

    first commit

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app= express();
 
 app.get("/",(req,res)=>{
-    res.send("Welcome to aws server 2")
+    res.send("Welcome to aws server 1")
 })
 
 app.listen(4400,()=>console.log("server running on 4400"))
